9f7424c4cdcd69a35492f8d53f77032411e924e2
hari <heyhariom24@gmail.com>
2023-06-26 00:34:29 +0530

    loop

---
+++ index.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i <= 20; i++) {
+//   const dots = ".".repeat(i);
+//   const left = 20 - i;
+//   const empty = " ".repeat(left);
+
+//   /* need to use  `process.stdout.write` becuase console.log print a newline character */
+//   /* \r clear the current line and then print the other characters making it looks like it refresh*/
+//   process.stdout.write(`\r[${dots}${empty}] ${i * 5}%`);
+// }
+
+
+let a 
